5e4b07a6e1b4ff5ce42f8eddcd06136d287df49d
jpayne-gaylor <jopayne@gaylor.com>
2026-06-19 11:01:08 -0400

    index.mjs

--- index.mjs
+++ index.mjs
@@ -19,12 +19,12 @@ const customIcon = L.icon({
 });
 
 // Markers
-L.marker([40.039003, -85.981196], { icon: customIcon }).addTo(
+L.marker([40.05517113654225, -85.98528012942833], { icon: customIcon }).addTo(
   map
 ).bindPopup(`
     <div class="popup">
       <img src="https://th.bing.com/th/id/OIP.h5BYzoCLipxtDQTDtko3FQHaHa?w=142&h=150&c=6&r=0&o=7&dpr=1.5&pid=1.7&rm=3" class="popup-img" />
-      <h3>Noblesville, IN</h3>
+      <h3>IND - 17230 Kraft Ct, Noblesville, IN 46060</h3>
       <p>Jeff Pea (PIC Manager)</p>
     </div>
   `);
@@ -34,7 +34,7 @@ L.marker([35.62245530831662, -80.77682864445171], { icon: customIcon }).addTo(
 ).bindPopup(`
     <div class="popup">
       <img src="https://images1.loopnet.com/i2/y74GHJBQUL7-iOm4U7jLyPfMaaLYrekWy3EQ6vBQk8U/112/208-Manufacturers-Blvd-Mooresville-NC-Primary-Photo-1-HighDefinition.jpg" class="popup-img" />
-      <h3>Mooresville, NC</h3>
+      <h3>CLT - 208 Manufacturers Blvd, Mooresville, NC 28115</h3>
       <p>Kevin Brennan (Innovation PM)</p>
     </div>
   `);
@@ -44,7 +44,7 @@ L.marker([37.3072235797037, -77.38041402905361], { icon: customIcon }).addTo(
 ).bindPopup(`
     <div class="popup">
       <img src="https://images1.loopnet.com/i2/zXq5gxAtVofPB9y6yIWv94_xlgR5ml15pHAifoQLpMM/110/1703-Ruffin-Mill-Rd-Colonial-Heights-VA-Primary-Photo-1-Large.jpg" class="popup-img" />
-      <h3>Richmond, VA</h3>
+      <h3>RIC - 1703 Ruffin Mill Rd, South Chesterfield, VA 23834</h3>
       <p>Gabriel Wiggins (PIC Manager)</p>
     </div>
   `);
@@ -54,7 +54,7 @@ L.marker([33.7609894450294, -84.56533010217149], { icon: customIcon }).addTo(
 ).bindPopup(`
     <div class="popup">
       <img src="https://images1.loopnet.com/i2/RJVctx4RAZg9HtcamS9mUQCc3BFgpCuRKCnvh1dr5Y8/116/335-Riverside-Pky-Austell-GA-Primary-Photo-1-LargeHighDefinition.jpg" class="popup-img" />
-      <h3>Austell, GA</h3>
+      <h3>ATL - 335 Riverside Pkwy, Austell, GA 30168</h3>
       <p>Colin Heron (Innovation Manager)</p>
     </div>
   `);
